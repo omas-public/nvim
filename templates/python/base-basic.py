391e41b5cfcd7f6dd065c28f3184f36aa8263198
Omas Naohiko <omas@std.it-college.ac.jp>
2026-03-19 13:46:34 +0900

    modify base-basic.py

--- templates/python/base-basic.py
+++ templates/python/base-basic.py
@@ -1,29 +1,27 @@
 from bisect import bisect
 from sys import stdin
 
+# utility function
 identity = I = lambda x: x
-consistent = K = lambda x: y: x
+consistent = K = lambda x: lambda y: x
+bisect_pick = lambda d, value: list(d.values())[bisect(list(d.keys()), value) - 1]
 join = lambda sep="\n", fn=str: lambda values: sep.join(map(fn, values))
 split = lambda sep=" ", fn=I: lambda v: list(map(fn, v.split(sep)))
 
-
-def bisect_pick(dic, value):
-    return list(dic.values())[bisect(list(dic.keys()), value) - 1]
-
-
+# 入力を受け取りリストにして返す関数
 def gets(n, fn=I):
     return [fn(line) for line in stdin.read().split('\n')[:n]]
 
-
+#　出力関数
 def puts(value, fn=str):
     print(fn(value))
 
-
+# 入力データを受取り加工して返す関数
 def main(p):
     buff = p
     return buff
 
-
+# 出力用に加工する関数
 def transform(p):
     buff = p
     return buff
@@ -32,10 +30,10 @@ def transform(p):
 if __name__ == "__main__":
 
     # preprocessing
-    _stdin = gets(1)
+    _in = gets(1)[0]
 
     # main function
-    _stdout = main(_stdin)
+    _out = main(_in)
 
     # display
-    puts(_stdout)
+    puts(transform(_out))
